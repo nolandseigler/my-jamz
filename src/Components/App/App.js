0e0b33afdf933f6d7e29a66e1ca8521f59efbb50
Noland Seigler <nolandseigler@gmail.com>
2020-05-15 15:39:17 -0500

    initial app skeleton

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults;';
+import PlayList from '../Playlist/Playlist';
 
 class App extends React.Component {
 
@@ -9,10 +12,10 @@ class App extends React.Component {
         <div>
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            <!-- Add a SearchBar component -->
+            <SearchBar />
             <div className="App-playlist">
-              <!-- Add a SearchResults component -->
-              <!-- Add a Playlist component -->
+              <SearchResults />
+              <PlayList />
             </div>
           </div>
         </div>
