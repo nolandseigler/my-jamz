fb63db1e5311414026489766026f98946cc88649
Noland Seigler <nolandseigler@gmail.com>
2020-05-16 00:04:49 -0500

    search bar term state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
       this.savePlaylist = this.savePlaylist.bind(this);
+      this.search = this.search.bind(this);
       this.updatePlaylistName = this.removeTrack.bind(this);
       this.state = {
           searchResults: [],
@@ -39,6 +40,10 @@ class App extends React.Component {
       const trackUris = this.state.playlistTracks.map(track => track.uri);
   }
 
+  search(term) {
+     alert(term);
+  }
+
   updatePlaylistName(name) {
       this.setState({ playlistName: name });
   }
@@ -48,7 +53,7 @@ class App extends React.Component {
         <div>
           <h1>Ja<span className="highlight">mmm</span>ing</h1>
           <div className="App">
-            <SearchBar />
+            <SearchBar onSearch={this.search} />
             <div className="App-playlist">
               <SearchResults onAdd={this.addTrack} searchResults={this.state.searchResults} />
               <Playlist onSave={this.savePlaylist} onNameChange={this.updatePlaylistName} onRemove={this.removeTrack} playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,10 +3,27 @@ import './SearchBar.css';
 
 class SearchBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.search = this.search.bind(this);
+        this.state = {
+            term: ''
+        }
+    }
+
+    handleTermChange(e) {
+        this.setState({ term: e.target.value });
+    }
+
+    search() {
+        this.props.onSearch(this.state.term)
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist"/>
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"/>
                 <button className="SearchButton">SEARCH</button>
             </div>
         );
