ac42fd2728ec41750edacad2302c970321888bbf
Noland Seigler <nolandseigler@gmail.com>
2020-05-17 23:15:48 -0500

    upon save i change state of playlist input component but it does not update back to New Playlist. I used setState() and even tried force

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 import Spotify from '../../util/Spotify';
 
+
 class App extends React.Component {
 
   constructor(props) {
